4932b768c55862510875e0bcab0d8a8201eb9e18
chumphreys0 <chumphreys@itba.edu.ar>
2026-08-25 19:25:04 -0300

    Cambios en clase trabajador

--- clases/trabajador.py
+++ clases/trabajador.py
@@ -9,7 +9,7 @@ class Trabajador:
         self.apellido=apellido
         self.fecha_nacimiento=fecha_nacimiento
         self.max_horas_semanales = max_horas_semanales
-        self.horas_asignadas = 0.0
+        self.horas_trabajadas = 0.0
         self.habilidades = [] #tambien se entiende como 'competencias' --> es una lista str
         self.credenciales = [] #lista de objetos de la clase Credencial
 
@@ -20,10 +20,18 @@ class Trabajador:
     def agregar_habilidades(self, habilidad: str): #por ahora lo consideramos una lista de aptitudes, ingresadas por el trabajador (ej.linkedin - aptitudes)
         self.habilidades.append(habilidad)
 
-    def tiene_credencial_activa(self, nombre_credencial: str, fecha: date) -> bool:
-       
+    def tiene_credencial_activa(self, nombre_credencial: str, fecha: date):
         pass
 
-    
+    def agregar_horas(self, horas:float): #idea: el main una vez que se llama al metodo aprobar de la clase asignacion, y si es true consecuentemente se llama a este metodo para agregar hs
+        self.horas_trabajadas +=horas   
+
+    def resetear_horas_semanales():
+        pass 
+
+    def excede_horas(self, horas:float): #horas proviene de asignacion (atributo del objeto)
+        if(self.horas_trabajadas+horas)>self.max_horas_semanales:
+            return True 
+
 
     
