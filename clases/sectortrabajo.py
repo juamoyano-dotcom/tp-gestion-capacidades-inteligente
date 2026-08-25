b5d66f9ba8ddc3c16f9e59f5d0076f3a5400e332
pepaconmembrillo <josemartinez@itba.edu.ar>
2026-08-25 19:51:58 -0300

    definicion sector trabajo

--- clases/sectortrabajo.py
+++ clases/sectortrabajo.py
@@ -4,19 +4,26 @@ from trabajador import Trabajador
 
 class SectorTrabajo:
     def __init__(self, id_sector: int, nombre: str, credenciales_obligatorias: List[str] = None, limite_personal: int = 0):
+
+        if limite_personal <  0:
+            raise ValueError("El límite de personal no puede ser negativo.")
+    
         self.id = id_sector
         self.nombre = nombre
         self.credenciales_obligatorias = credenciales_obligatorias if credenciales_obligatorias is not None else []
         self.limite_personal = limite_personal
 
-    def trabajador_cumple_credenciales(self, trab, fecha: date) -> bool:
-        """
-        Verifica si el trabajador posee todas las credenciales obligatorias 
-        del sector activas a la fecha indicada.
-        """
-        # Si el área no exige credenciales, cumple automáticamente
+    def trabajador_cumple_credenciales(self, trab: Trabajador, fecha: date):
+
         if not self.credenciales_obligatorias:
             return True
-        
-        # Se apoya en el método 'credenciales_activas' de la clase Trabajador
-        return trab.credenciales_activas(self.credenciales_obligatorias, fecha)
+
+        credenciales_activas_trabajador = [
+            c.nombre for c in trab.credenciales if c.esta_activa(fecha)
+        ]
+
+        for obligatoria in self.credenciales_obligatorias:
+            if obligatoria not in credenciales_activas_trabajador:
+                return False  
+
+        return True  
