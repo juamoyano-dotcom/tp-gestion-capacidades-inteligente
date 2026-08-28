431be3fdb44c33871f311f958570b8c3c0e9306f
chumphreys0 <chumphreys@itba.edu.ar>
2026-08-28 17:07:12 -0300

    Arreglo lo del issue 4 en credencial

--- clases/credencial.py
+++ clases/credencial.py
@@ -1,11 +1,16 @@
-
 from datetime import date
 
 class Credencial:
     def __init__(self, nombre: str, fecha_obtencion: date, fecha_expiracion: date):
+
+        if fecha_obtencion > fecha_expiracion:
+            raise ValueError("La fecha de obtención no puede ser posterior a la fecha de expiración.")
+            #Agrego esto del issue4, para que no se acepten fechas invertidas. 
+
         self.nombre = nombre
         self.fecha_obtencion = fecha_obtencion
         self.fecha_expiracion = fecha_expiracion
 
     def esta_activa(self, fecha_consulta: date):
         return self.fecha_obtencion <= fecha_consulta <= self.fecha_expiracion
+    
