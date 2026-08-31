3ed1408e760f3a1355daafc148d194eee22a286e
Kenio-glicht <rdangelogonzalez@itba.edu.ar>
2026-08-31 11:19:11 -0300

    Implementa Enum para franjas horarias

--- clases/franjahoraria.py
+++ clases/franjahoraria.py
@@ -2,37 +2,33 @@ from datetime import time
 from enum import Enum
 
 
+class Franja(Enum): 
+#el dominio esta definido directamente, python se encarga de validarlo
+
+    MAÑANA = (1, "Mañana") 
+    TARDE = (2, "Tarde") 
+    NOCHE = (3, "Noche")
+
+    def __init__(self, id_franja, descripcion): 
+        self.id_franja = id_franja 
+        self.descripcion = descripcion
+
+
 class FranjaHoraria:
 
-    FRANJAS_VALIDAS = {
-        1: "Mañana",
-        2: "Tarde",
-        3: "Noche"
-    }
+    def __init__(self, franja: Franja, hora_inicio: time, hora_fin: time):
 
-
-    def __init__(self, id_franja: int, descripcion: str, hora_inicio: time, hora_fin: time):
-
-        self.id_franja = id_franja
-        self.descripcion = descripcion
-        self.hora_inicio = hora_inicio
+        self.franja = franja 
+        self.hora_inicio = hora_inicio 
         self.hora_fin = hora_fin
 
-        #si llamas a estos dos metodos, cuando creas el objeto se verifica automaticamente la hora y franja horaria
+        #si llamas a este metodo, cuando creas el objeto se verifica automaticamente la hora 
         self.validar_hora()
-        self.validar_franja()
 
 
-    def validar_hora(self):
-        if self.hora_inicio == self.hora_fin:
-            raise ValueError("La hora de inicio debe ser distinta a la hora de fin.")
+def validar_hora(self): 
+    if self.hora_inicio == self.hora_fin: 
+        raise ValueError( "La hora de inicio debe ser distinta a la hora de fin." )
 
-    def validar_franja(self):
-        if self.id_franja not in self.FRANJAS_VALIDAS:
-            raise ValueError("El id de la franja horaria debe ser 1, 2 o 3.")
-        if self.FRANJAS_VALIDAS[self.id_franja] != self.descripcion:
-            raise ValueError(f"Para el id {self.id_franja}, la descripción debe ser '{self.FRANJAS_VALIDAS[self.id_franja]}'.")
 
-        
-    
-        
+
